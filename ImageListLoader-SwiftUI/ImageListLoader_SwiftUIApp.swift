308392213d67f4f5d648a5697c1ecddfe3f73316
Stanley Darmawan <stanley.darmawan@lexicondigital.com.au>
2024-05-22 10:25:04 +0700

    Initial Commit

---
+++ ImageListLoader-SwiftUI/ImageListLoader_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageListLoader_SwiftUIApp.swift
+//  ImageListLoader-SwiftUI
+//
+//  Created by Stanley Darmawan on 22/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageListLoader_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
